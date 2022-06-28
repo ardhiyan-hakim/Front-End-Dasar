f8364a9bd4b02ef96ddcbcae6e829bf94c53e906
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-06-28 15:07:09 +0700

    Rendering Content With Javascript

---
+++ index.js
@@ -0,0 +1,40 @@
+import data from "./scripts/data.js"
+
+window.addEventListener("load", () => {
+  renderElement();
+});
+
+const patienceBtn = document.querySelector("#patience-button");
+patienceBtn.addEventListener("click", () => {
+  scrollTo(0, 215);
+});
+
+const trustBtn = document.querySelector("#trust-button");
+trustBtn.addEventListener("click", () => {
+  scrollTo(0, 460);
+});
+
+const consistencyBtn = document.querySelector("#consistency-button");
+consistencyBtn.addEventListener("click", () => {
+  scrollTo(0, 830);
+});
+
+function renderElement() {
+  const dataElement = data;
+  console.log(dataElement);
+
+  const contentCard = document.querySelector('.content-card');
+
+  dataElement.forEach((data) => {
+    const h2 = document.createElement('h2');
+    h2.setAttribute('id', data.id);
+    h2.innerText = data.headingContent;
+    contentCard.append(h2)
+
+    data.textContent.paragraph.forEach((paragraph) => {
+      const p = document.createElement('p');
+      p.innerText = paragraph;
+      contentCard.append(p)
+    })
+  })
+}
---
+++ scripts/data.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+    id: 'patience',
+    headingContent: "Patience",
+    textContent: {
+      paragraph: [
+        "It is important to be patient and keep going to reach your goals. Once we learn the importance of patience, we can stop worrying when things take longer than we would like. This applies to small problems like being stuck in traffic jams as well as bigger issues such as getting your dream job. Patience makes it easier to live because it reduces negative emotions like stress, anger, or frustration. When one is patient, one is calmer and more open to changes.",
+      ],
+    },
+  },
+  {
+    id: 'trust',
+    headingContent: "Trust",
+    textContent: {
+      paragraph: [
+        "Trust allowing people to live and work together, feel safe and belong to a group. Trust in a leader allows organizations and communities to flourish, while the absence of trust can cause fragmentation, conflict and even war. That’s why we need to trust our leaders, our family members, our friends and our co-workers, albeit in different ways.",
+
+        "Loss of trust could causing us to withdraw our energy and level of engagement. As a result, we pull back from that person and no longer feel part of their world. But on the positive side, trust makes people feel eager to be part of a relationship or group, with a shared purpose and a willingness to depend on each other. When trust is intact, we will willingly contribute what is needed, not just by offering our presence, but also by sharing our dedication, talent, energy and honest thoughts on how the relationship or group is working."
+      ],
+    },
+  },
+  {
+    id: 'consistency',
+    headingContent: "Consistency",
+    textContent: {
+      paragraph: [
+        "Consistency is a necessary ingredient in achieving long term goals and without consistency we'll just running around with a little or not even a slightest result. When you do something consistently over time, you’ll notice that the new habit will gradually replace the old one. And it becomes a routine. One way to become disciplined is to consistently do a thing over a long time. We can’t train ourself to do things in a controlled manner if we don’t do it repeatedly. And we can’t be consistent if you aren’t disciplined. ",
+      ],
+    },
+  },
+  {
+    id: 'conclusion',
+    headingContent: "Conclusion",
+    textContent: {
+      paragraph: [
+        "We had a long journey ahead of us, and we need those 3 element to keep walking that journey or even running on our own. We don't really need to be in a hurry, but we need to remember that times are limited, so by learning what the importance of patience, trust, and consistency i hope you guys would understand that every could be changed, no matter how bad we or our circumstances are right now.",
+      ],
+    },
+  },
+]
+
+export default data;
