6fcb688284ee8dd356e6840ba9961bf927276c71
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-07-01 19:42:54 +0700

    Recoloring and Add Functionality to Each Button Inside

--- index.js
+++ index.js
@@ -2,40 +2,54 @@ import data from "./scripts/data.js";
 
 window.addEventListener("load", () => {
   renderElement();
+  scrollToTop();
+  getYValue().then((result) => {
+    const navBtnList = document.querySelectorAll(".navigation-button");
+
+    navBtnList.forEach((navElement) => {
+      let height;
+      result.forEach((resultItem) => {
+        if (navElement.classList.contains(resultItem.id)) {
+          height = resultItem.position;
+        }
+      });
+
+      navElement.addEventListener("click", () => {
+        scrollTo(0, height - 50);
+      });
+    });
+  });
 });
 
-const patienceBtn = document.querySelector("#patience-button");
-patienceBtn.addEventListener("click", () => {
-  scrollTo(0, 215);
-});
+window.addEventListener('resize', () => {
+  location.reload();
+})
 
-const trustBtn = document.querySelector("#trust-button");
-trustBtn.addEventListener("click", () => {
-  scrollTo(0, 460);
-});
+function scrollToTop() {
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+  }, 100);
+}
 
-const consistencyBtn = document.querySelector("#consistency-button");
-consistencyBtn.addEventListener("click", () => {
-  scrollTo(0, 830);
-});
-
-const menuBtn = document.querySelector(".menu-button");
-menuBtn.addEventListener("click", () => {
-  console.log('Sedang menekan menu button');
-  const menuCtn = document.querySelector(".menu_screen-container");
-  menuBtn.classList.toggle('show')
-  menuCtn.classList.toggle('show')
-});
+function getYValue() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(checkHeightElement());
+    }, 1000);
+  });
+}
 
 const modals = document.querySelector(".modals");
 const header = document.querySelector("header");
 const main = document.querySelector("main");
+const menuScreen = document.querySelector(".menu_screen-container");
 
 const signInBtn = document.querySelector(".sign_in-button");
 signInBtn.addEventListener("click", () => {
   modals.classList.add("show");
   header.classList.add("blur");
   main.classList.add("blur");
+  menuScreen.classList.add("blur");
 });
 
 const exitModalsBtn = document.querySelector(".modals-header_exit i");
@@ -43,7 +57,16 @@ exitModalsBtn.addEventListener("click", () => {
   modals.classList.remove("show");
   header.classList.remove("blur");
   main.classList.remove("blur");
+  menuScreen.classList.remove("blur");
 });
+
+const signInMenuBtn = document.querySelector('.navigation-button.sign-in');
+signInMenuBtn.addEventListener('click', () => {
+  modals.classList.add("show");
+  header.classList.add("blur");
+  main.classList.add("blur");
+  menuScreen.classList.add("blur");
+})
 
 const navList = document.querySelectorAll(".navigation-item");
 
@@ -60,6 +83,39 @@ function iterateNav(list) {
       item.classList.remove("active");
     }
   });
+}
+
+const menuBtn = document.querySelector(".menu-button");
+menuBtn.addEventListener("click", () => {
+  const menuCtn = document.querySelector(".menu_screen-container");
+  menuBtn.classList.toggle("show");
+  menuCtn.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+  const menuCtn = document.querySelector(".menu_screen-container");
+  menuBtn.classList.remove("show");
+  menuCtn.classList.remove("show");
+});
+
+function checkHeightElement() {
+  const patienceEl = document.querySelector("#patience");
+  const trustEl = document.querySelector("#trust");
+  const consistEl = document.querySelector("#consistency");
+
+  const arrayEl = [patienceEl, trustEl, consistEl];
+  const arrayObjEl = [];
+
+  arrayEl.forEach((element) => {
+    const position = element.getBoundingClientRect();
+    const object = {};
+
+    object.name = element;
+    object.position = position.top;
+    object.id = element.id;
+    arrayObjEl.push(object);
+  });
+  return arrayObjEl;
 }
 
 function renderElement() {
