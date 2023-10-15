1d9b0fe90f2465c011d0263891df13dbab614c07
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2023-10-15 14:57:38 +0700

    Readjust layout of Menu Screen and Modals, Changing into put empty Article element into HTML file

--- index.js
+++ index.js
@@ -41,31 +41,35 @@ function getYValue() {
 
 const modals = document.querySelector(".modals");
 const header = document.querySelector("header");
-const main = document.querySelector("main");
+const mainWrapper = document.querySelector(".main-wrapper");
 const menuScreen = document.querySelector(".menu_screen-container");
+const navigationContainer = document.querySelector(".navigation-container");
 
 const signInBtn = document.querySelector(".sign_in-button");
 signInBtn.addEventListener("click", () => {
   modals.classList.add("show");
   header.classList.add("blur");
-  main.classList.add("blur");
+  mainWrapper.classList.add("blur");
   menuScreen.classList.add("blur");
+  navigationContainer.classList.add("blur");
 });
 
 const exitModalsBtn = document.querySelector(".modals-header_exit i");
 exitModalsBtn.addEventListener("click", () => {
   modals.classList.remove("show");
   header.classList.remove("blur");
-  main.classList.remove("blur");
+  mainWrapper.classList.remove("blur");
   menuScreen.classList.remove("blur");
+  navigationContainer.classList.remove("blur");
 });
 
 const signInMenuBtn = document.querySelector('.navigation-button.sign-in');
 signInMenuBtn.addEventListener('click', () => {
   modals.classList.add("show");
   header.classList.add("blur");
-  main.classList.add("blur");
+  mainWrapper.classList.add("blur");
   menuScreen.classList.add("blur");
+  navigationContainer.classList.add("blur");
 })
 
 const navList = document.querySelectorAll(".navigation-item");
@@ -120,22 +124,23 @@ function checkHeightElement() {
 
 function renderElement() {
   const dataElement = data;
+  const articleContents = document.querySelectorAll("article");
   const contentCard = document.querySelector(".content-card");
   
-  dataElement.forEach((data) => {
-    const article = document.createElement('article');
+  articleContents.forEach((articleContent, index) => {
     const h2 = document.createElement("h2");
-    h2.setAttribute("id", data.id);
-    h2.innerText = data.headingContent;
-    article.append(h2);
-    
-    data.textContent.paragraph.forEach((paragraph) => {
+    h2.setAttribute("id", dataElement[index].id);
+    h2.innerText = dataElement[index].headingContent;
+    articleContent.append(h2);
+
+
+    dataElement[index].textContent.paragraph.forEach((paragraph) => {
       const p = document.createElement("p");
       p.innerText = paragraph;
-      article.append(p);
+      articleContent.append(p);
     });
     
-    contentCard.append(article);
-  });
+    contentCard.append(articleContent);
+  })
 
 }
